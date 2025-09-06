5bac0b4ef1fd337f3fa5dd6cd89b21afdcb5097c
Alejandro <alejopm29@gmail.com>
2025-09-06 12:39:38 -0500

    Feat: Adjust images configs

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    unoptimized: false,
+    unoptimized: true,
     domains: [],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 120, 128, 150, 200, 256, 300, 384, 400, 450, 500, 600],
