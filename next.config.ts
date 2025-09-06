035db201e50dd4df66eb9b94689354b92ee5cba0
Alejandro <alejopm29@gmail.com>
2025-09-06 12:24:32 -0500

    Config: Add imageSize to NextConfig

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     unoptimized: false,
     domains: [],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    imageSizes: [16, 32, 48, 64, 96, 120, 128, 150, 200, 256, 300, 384, 400, 450, 500, 600],
     formats: ['image/webp', 'image/avif'],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
